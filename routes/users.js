2f17f6c927ef6bd2db0f0af164f8ed0c58a4f909
Chau Le <nganchau.lenguyen@gmail.com>
2018-11-22 19:21:11 +0700

    modify users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const router = express.Router();
+const UserController = require('../src/user/UserController');
+
+router.get('/info', UserController.user_info_get);
+
+router.get('/register', UserController.user_register_get);
+router.post('/register', UserController.user_register_post);
+
+router.get('/login', UserController.user_login_get);
+// router.post('/login', UserController.user_register_post);
+
+router.get('/logout', UserController.user_logout_get);
 
 module.exports = router;
