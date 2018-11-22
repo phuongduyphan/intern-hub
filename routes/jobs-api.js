2f8056994f011d2a4cda8230b4afb982cc59d0aa
Chau Le <nganchau.lenguyen@gmail.com>
2018-11-22 12:47:00 +0700

    modify api of jobs and recruiters

--- routes/jobs-api.js
+++ routes/jobs-api.js
@@ -7,4 +7,12 @@ router.get('/', JobController.jobs_get);
 
 router.get('/search', JobController.jobs_search_get);
 
+router.get('/:jobId', JobController.jobId_get);
+
+router.get('/:jobId/info', JobController.jobId_info_get);
+
+router.put('/:jobId/status', JobController.jobId_status_put);
+
+router.get('/:jobId/categories', JobController.jobId_categories_get);
+
 module.exports = router;
--- routes/recruiters-api.js
+++ routes/recruiters-api.js
@@ -13,12 +13,4 @@ router.post('/:recruiterId/info', RecruiterController.recruiterId_info_post);
 router.get('/:recruiterId/jobs', RecruiterController.recruiterId_jobs_get);
 router.post('/:recruiterId/jobs', RecruiterController.recruiterId_jobs_post);
 
-router.get('/:recruiterId/jobs/:jobId', RecruiterController.recruiterId_jobId_get);
-
-router.put('/:recruiterId/jobs/:jobId/status', RecruiterController.recruiterId_jobId_status_put);
-
-router.get('/:recruiterId/jobs/:jobId/info', RecruiterController.recruiterId_jobId_info_get);
-
-router.get('/:recruiterId/jobs/:jobId/categories', RecruiterController.recruiterId_jobId_categories_get);
-
 module.exports = router;
