7263958578bc1b6ff006e663f113867769d17e9f
Thanh Tung <tungtoitiu@gmail.com>
2018-11-22 08:32:29 +0700

    ...

--- client/src/components/layout/Appbar.js
+++ client/src/components/layout/Appbar.js
@@ -2,8 +2,6 @@ import AppBar from '@material-ui/core/AppBar';
 import React from 'react'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Copyright from '@material-ui/icons/Copyright';
-import Typography from '@material-ui/core/Typography';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
