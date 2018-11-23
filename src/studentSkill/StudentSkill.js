e847b93e97974bb58386a3296071e2ad59994a92
Duy Phan <phuongduyphan@gmail.com>
2018-11-23 11:05:58 +0700

    add search in StudentService

---
+++ src/studentSkill/StudentSkill.js
@@ -0,0 +1,35 @@
+const { Model } = require('../../config/mysql/mysql-config');
+
+class StudentSkill extends Model {
+  static get tableName() {
+    return 'studentSkill';
+  }
+
+  static get relationMappings() {
+    const { Student } = require('../student/Student');
+    const { Skill } = require('../skill/Skill');
+
+    return {
+      users: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Student,
+        join: {
+          from: 'studentSkill.studentId',
+          to: 'students.studentId',
+        },
+      },
+      skills: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Skill,
+        join: {
+          from: 'studentSkill.skillId',
+          to: 'skills.skillId',
+        },
+      },
+    };
+  }
+}
+
+module.exports = {
+  StudentSkill,
+};
