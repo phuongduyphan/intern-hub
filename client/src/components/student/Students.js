cabaa4c8f3083886af35d27986449e30af97b93f
nhandoantrong <nhandoantrong17498@gmail.com>
2018-11-25 04:51:57 +0700

    pushing

--- client/src/components/student/Students.js
+++ client/src/components/student/Students.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
@@ -10,6 +10,9 @@ import Work from '@material-ui/icons/Work';
 import Email from '@material-ui/icons/Email';
 import Place from '@material-ui/icons/Place';
 import Chip from '@material-ui/core/Chip';
+import MediaQuery from 'react-responsive';
+import { Modal} from 'react-bootstrap';
+
 
 const styles = theme => ({
   root: {
@@ -17,6 +20,9 @@ const styles = theme => ({
     maxWidth: 600,
     padding: theme.spacing.unit * 2,
     margin: 5
+  },
+  studentName: {
+    cursor: "pointer"
   },
   image: {
     width: 80,
@@ -46,8 +52,26 @@ const styles = theme => ({
   },
 });
 
-const Students = (props) => {
-  const { classes, student, avatar } = props;
+class Students extends Component {
+  constructor(props) {
+    super(props);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+    this.state = {
+      show: false
+    };
+  }
+
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
+  render(){
+  const { classes, student, avatar } = this.props;
+
 
   const renderSkill = (
     <div>
@@ -66,6 +90,9 @@ const Students = (props) => {
   );
  
   return (
+    <div>
+    <MediaQuery minDeviceWidth={1000}>
+
     <Grid container direction='row' alignContent='center' justify='center'>
       <Paper className={classes.root }>
         <Grid container spacing={16}>
@@ -74,27 +101,30 @@ const Students = (props) => {
           </Grid>
           <Grid item xs={10} sm container>
             <Grid item xs container direction="column" spacing={16}>
-              <Grid item xs>
-                <Typography variant='headline' color='primary'>{student.users.displayname}</Typography>
+              <Grid item xs className={classes.studentName} onClick={this.handleShow}>
+                <Typography variant='headline' color='primary' 
+                
+                
+                >{student.users.displayname}</Typography>
                 {renderSkill}
               </Grid>
 
               <Grid item>
                 <Grid container>
                   <Grid item xs={4}>
-                    <Grid container>
-                      <Grid item><Work className={classes.icon}/></Grid>
-                      <Grid item><Typography variant="body1" color="textSecondary">{student.studentCollege}</Typography></Grid>
+                    <Grid container className="studentItem">
+                      <div ><Work className={classes.icon}/></div>
+                      <div ><Typography variant="body1" color="textSecondary">{student.studentCollege}</Typography></div>
                     </Grid>
                   </Grid>
                   <Grid item xs={4}>
-                    <Grid container>
+                    <Grid container className="studentItem">
                       <Grid item><Place className={classes.icon}/></Grid>
                       <Grid item><Typography variant="body1" color="textSecondary">{student.studentMajor}</Typography></Grid>
                     </Grid>
                   </Grid>
                   <Grid item xs={4}>
-                    <Grid container>
+                    <Grid container className="studentItem">
                       <Grid item><Email className={classes.icon}/></Grid>
                       <Grid item><Typography variant="body1" color="textSecondary">{student.email}</Typography></Grid>
                     </Grid>
@@ -114,11 +144,39 @@ const Students = (props) => {
         </Grid>
       </Paper>
     </Grid>  
+    </MediaQuery>
+    <MediaQuery maxDeviceWidth={1000}>
+        <div class="card" >
+          <img class="card-img-top" src={avatar} />
+          <div class="card-body">
+            <h4 class="card-title">{student.users.displayname}</h4>
+            <p class="card-text">{renderSkill}</p>
+            <div class="d-flex">
+              <a href="/" class="btn btn-primary">See Profile</a>
+              <a href="/" class="btn btn-success ml-auto">Invite</a>
+            </div>
+
+          </div>
+        </div>
+      </MediaQuery>
+
+
+      <Modal show={this.state.show} onHide={this.close}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Modal body
+         </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.close}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
-
+}
 Students.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-
 export default withStyles(styles)(Students);
